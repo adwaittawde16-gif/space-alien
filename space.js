a36dd3e8f754aef4630cb8b587165da5d620dbf3
adwaittawde16-gif <adwaittawde16@gmail.com>
2026-03-06 20:23:47 +0530

    Add files via upload

---
+++ space.js
@@ -0,0 +1,64 @@
+let tileSize =32;
+let rows = 16;
+let columns =16;
+
+let board;
+let boardWidth = tileSize * columns;
+let boardHeight = tileSize* rows;
+let context;
+
+
+
+let shipWidth=tileSize*2;
+let shipHeight=tileSize;
+let shipX=tileSize*columns/2-tileSize
+let shipeY= tileSize*rows-tileSize*2;
+
+
+let ship={
+    x :shipX,
+    y:shipeY,
+    width: shipWidth,
+    height:shipHeight
+}
+
+let shipImg;
+let shipVelocityX = tileSize;
+
+window.onload = function()
+{
+    board = document.getElementById("board");
+    board.width=boardWidth;
+    board.height=boardHeight;
+    context= board.getContext("2d");
+
+
+    //context.fillStyle = "green";
+    //context.fillRect(ship.x,ship.y,ship.width,ship.height);
+
+    shipImg = new Image();
+    shipImg.src= "./ship.png";
+    shipImg.onload=function(){
+    context.drawImage(shipImg,ship.x,ship.y,ship.width,ship.height);
+    }
+    requestAnimationFrame(update);
+    document.addEventListener("keydown",moveShip);
+}
+
+function update()
+{
+    requestAnimationFrame(update);
+    context.clearRect(0,0,boardHeight,boardWidth);
+    context.drawImage(shipImg,ship.x,ship.y,ship.width,ship.height);
+}
+
+function moveShip(e)
+{
+   if(e.code == "ArrowLeft" && ship.x - shipVelocityX >=0)
+   {
+    ship.x -= shipVelocityX;
+   }
+   else if (e.code == "ArrowRight" && ship.x + shipVelocityX + ship.width <= board.width ){
+    ship.x += shipVelocityX;
+   }
+}
